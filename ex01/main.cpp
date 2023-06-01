b39c28a212c789dd41043e59a88d997ccf8a3c45
Nazar <nizarbonex1@gmail.com>
2023-06-01 23:12:26 +0200

    fin

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: naal-jen <naal-jen@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:49:20 by naal-jen          #+#    #+#             */
-/*   Updated: 2023/05/31 17:00:22 by naal-jen         ###   ########.fr       */
+/*   Updated: 2023/06/01 23:06:17 by naal-jen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	main(void)
 {
+	int			x = 0;
 	int			i = 0;
 	int			j = 0;
 	int			index = 0;
@@ -22,9 +23,31 @@ int	main(void)
 
 	while (1)
 	{
-		std::cout << "you have three options: 1.ADD 2.SEARCH 3. EXIT" << std::endl;
-		std::cout << "choose carfully" << std::endl;
-		std::cin >> input;
+		std::cout << "options: 1.ADD 2.SEARCH 3. EXIT" << std::endl;
+		if (!input.empty())
+			input.clear();
+		while (input.empty())
+		{
+			std::getline(std::cin, input);
+			if (x == 1)
+				x++;
+			else if (std::cin.eof())
+				exit(1);
+			else
+			{
+				while(!input.empty() && std::isspace(input[0]))
+				{
+					input.erase(input.begin());
+				}
+				while(!input.empty() && std::isspace(input[input.size() - 1]))
+				{
+					input.resize(input.size() - 1);
+				}
+				if (input.empty()) {
+					std::cout << "Invalid input, please try again." << std::endl;
+				}
+			}
+		}
 		if (input == "ADD")
 		{
 			if (i > 7)
@@ -59,6 +82,7 @@ int	main(void)
 			std::cout << phone.contacts[index].phone_number_mod << std::endl;
 			std::cout << "Darkest secret is" << std::endl;
 			std::cout << phone.contacts[index].darkest_secret_mod << std::endl;
+			x = 1;
 		}
 		else if (input == "EXIT")
 			phone.Exit();
--- ex01/phone.class.cpp
+++ ex01/phone.class.cpp
@@ -6,7 +6,7 @@
 /*   By: naal-jen <naal-jen@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:17:04 by naal-jen          #+#    #+#             */
-/*   Updated: 2023/05/31 19:47:59 by naal-jen         ###   ########.fr       */
+/*   Updated: 2023/06/01 22:48:08 by naal-jen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@
 
 void	PhoneBook::Add(int i)
 {
-
-	int	j = 0;
 	std::cout << "What is your first name?" << std::endl;
+	if (!this->contacts[i].first_name.empty())
+		this->contacts[i].first_name.clear();
 	while (this->contacts[i].first_name.empty())
 	{
 		std::getline(std::cin, this->contacts[i].first_name);
-		if (this->contacts[i].first_name.empty() && j == 0)
-			j++;
-		else if (std::cin.eof())
+		if (std::cin.eof())
 			exit(1);
 		else
 		{
@@ -43,47 +41,131 @@ void	PhoneBook::Add(int i)
 		}
 	}
 
-
 	std::cout << "What is you'r last_name?" << std::endl;
-	std::cin >> this->contacts[i].last_name;
+	if (!this->contacts[i].last_name.empty())
+		this->contacts[i].last_name.clear();
+	while (this->contacts[i].last_name.empty())
+	{
+		std::getline(std::cin, this->contacts[i].last_name);
+		if (std::cin.eof())
+			exit(1);
+		else
+		{
+			while(!this->contacts[i].last_name.empty() && std::isspace(this->contacts[i].last_name[0]))
+			{
+				this->contacts[i].last_name.erase(this->contacts[i].last_name.begin());
+			}
+			while(!this->contacts[i].last_name.empty() && std::isspace(this->contacts[i].last_name[this->contacts[i].last_name.size() - 1]))
+			{
+				this->contacts[i].last_name.resize(this->contacts[i].last_name.size() - 1);
+			}
+			if (this->contacts[i].last_name.empty()) {
+				std::cout << "Invalid input, please try again." << std::endl;
+			}
+		}
+	}
 
 	std::cout << "What is you'r nickname?" << std::endl;
-	std::cin >> this->contacts[i].nickname;
+	if (!this->contacts[i].nickname.empty())
+		this->contacts[i].nickname.clear();
+	while (this->contacts[i].nickname.empty())
+	{
+		std::getline(std::cin, this->contacts[i].nickname);
+		if (std::cin.eof())
+			exit(1);
+		else
+		{
+			while(!this->contacts[i].nickname.empty() && std::isspace(this->contacts[i].nickname[0]))
+			{
+				this->contacts[i].nickname.erase(this->contacts[i].nickname.begin());
+			}
+			while(!this->contacts[i].nickname.empty() && std::isspace(this->contacts[i].nickname[this->contacts[i].nickname.size() - 1]))
+			{
+				this->contacts[i].nickname.resize(this->contacts[i].nickname.size() - 1);
+			}
+			if (this->contacts[i].nickname.empty()) {
+				std::cout << "Invalid input, please try again." << std::endl;
+			}
+		}
+	}
 
 	std::cout << "What is you'r phone_number?" << std::endl;
-	std::cin >> this->contacts[i].phone_number;
+	if (!this->contacts[i].phone_number.empty())
+		this->contacts[i].phone_number.clear();
+	while (this->contacts[i].phone_number.empty())
+	{
+		std::getline(std::cin, this->contacts[i].phone_number);
+		if (std::cin.eof())
+			exit(1);
+		else
+		{
+			while(!this->contacts[i].phone_number.empty() && std::isspace(this->contacts[i].phone_number[0]))
+			{
+				this->contacts[i].phone_number.erase(this->contacts[i].phone_number.begin());
+			}
+			while(!this->contacts[i].phone_number.empty() && std::isspace(this->contacts[i].phone_number[this->contacts[i].phone_number.size() - 1]))
+			{
+				this->contacts[i].phone_number.resize(this->contacts[i].phone_number.size() - 1);
+			}
+			if (this->contacts[i].phone_number.empty()) {
+				std::cout << "Invalid input, please try again." << std::endl;
+			}
+		}
+	}
 
 	std::cout << "What is you'r darkest_secret?" << std::endl;
-	std::cin >> this->contacts[i].darkest_secret;
+	if (!this->contacts[i].darkest_secret.empty())
+		this->contacts[i].darkest_secret.clear();
+	while (this->contacts[i].darkest_secret.empty())
+	{
+		std::getline(std::cin, this->contacts[i].darkest_secret);
+		if (std::cin.eof())
+			exit(1);
+		else
+		{
+			while(!this->contacts[i].darkest_secret.empty() && std::isspace(this->contacts[i].darkest_secret[0]))
+			{
+				this->contacts[i].darkest_secret.erase(this->contacts[i].darkest_secret.begin());
+			}
+			while(!this->contacts[i].darkest_secret.empty() && std::isspace(this->contacts[i].darkest_secret[this->contacts[i].darkest_secret.size() - 1]))
+			{
+				this->contacts[i].darkest_secret.resize(this->contacts[i].darkest_secret.size() - 1);
+			}
+			if (this->contacts[i].darkest_secret.empty()) {
+				std::cout << "Invalid input, please try again." << std::endl;
+			}
+		}
+	}
+
+	this->contacts[i].first_name_mod = this->contacts[i].first_name;
+	this->contacts[i].last_name_mod = this->contacts[i].last_name;
+	this->contacts[i].nickname_mod = this->contacts[i].nickname;
+	this->contacts[i].phone_number_mod = this->contacts[i].phone_number;
+	this->contacts[i].darkest_secret_mod = this->contacts[i].darkest_secret;
 
 
 	if (this->contacts[i].first_name.length() > 10)
 	{
-		this->contacts[i].first_name_mod = this->contacts[i].first_name;
 		this->contacts[i].first_name.resize(10);
 		this->contacts[i].first_name[9] = '.';
 	}
 	if (this->contacts[i].last_name.length() > 10)
 	{
-		this->contacts[i].last_name_mod = this->contacts[i].last_name;
 		this->contacts[i].last_name.resize(10);
 		this->contacts[i].last_name[9] = '.';
 	}
 	if (this->contacts[i].nickname.length() > 10)
 	{
-		this->contacts[i].nickname_mod = this->contacts[i].nickname;
 		this->contacts[i].nickname.resize(10);
 		this->contacts[i].nickname[9] = '.';
 	}
 	if (this->contacts[i].phone_number.length() > 10)
 	{
-		this->contacts[i].phone_number_mod = this->contacts[i].phone_number;
 		this->contacts[i].phone_number.resize(10);
 		this->contacts[i].phone_number[9] = '.';
 	}
 	if (this->contacts[i].darkest_secret.length() > 10)
 	{
-		this->contacts[i].darkest_secret_mod = this->contacts[i].darkest_secret;
 		this->contacts[i].darkest_secret.resize(10);
 		this->contacts[i].darkest_secret[9] = '.';
 	}
